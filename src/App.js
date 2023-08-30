79f9e3055aece59e70efac005a05ade104c0dbe1
Zsolt Nagy <info@zsoltnagy.eu>
2023-08-31 01:59:51 +0200

    Hard coded state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import ParkingLotForm from "./Components/ParkingLotForm/ParkingLotForm";
 import ParkingLotList from "./Components/ParkingLotList/ParkingLotList";
 
 import "./App.css";
 
 function App() {
+    let [parkingLotItems, setParkingLotItems] = useState([
+        {
+            date: "2023-06-27",
+            priority: "Low",
+            link: "https://google.com/",
+            description: "This is where you can find the ultimate source of truth.",
+        },
+        {
+            date: "2023-08-29",
+            priority: "Medium",
+            link: "https://react.dev/",
+            description: "React documentation and tutorial",
+        },
+    ]);
+
     return (
         <div className="App">
             <header>
