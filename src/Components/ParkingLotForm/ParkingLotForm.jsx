a180ede18416fb326bfd1d00b009596e22e70b62
Zsolt Nagy <info@zsoltnagy.eu>
2023-09-09 23:43:32 +0200

    add item

--- src/Components/ParkingLotForm/ParkingLotForm.jsx
+++ src/Components/ParkingLotForm/ParkingLotForm.jsx
@@ -9,7 +9,7 @@ const PRIORITIES = {
   High: 'High'
 }
 
-export default function ParkingLotForm() {
+export default function ParkingLotForm({ addItem }) {
 
   const [date, setDate] = useState('');
   const [link, setLink] = useState('');
@@ -37,7 +37,16 @@ export default function ParkingLotForm() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(date, link, description, priority);
+
+    const [y, M, d] = date.split('-');  
+    const formattedDate = `${M}/${d}/${y}`;
+
+    addItem(formattedDate, link, description, priority);
+
+    setDate('');
+    setLink('');
+    setDescription('');
+    setPriority(PRIORITIES.Medium);    
   }
 
   return (
